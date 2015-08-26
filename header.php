328b0c8fae97e336202ed920d911358fe22d11b2
melissacabral <melissacabral@gmail.com>
2015-08-26 09:01:52 -0700

    Update header.php

--- header.php
+++ header.php
@@ -5,10 +5,7 @@
 	<meta name="viewport" content="width=device-width" />
 	<title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<!-- HTML5 shiv -->
-	<!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-	<![endif]-->
+
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ); ?>/styles/reset.css" />
 	<?php 
 	//Necessary in <head> for JS and plugins to work. 
